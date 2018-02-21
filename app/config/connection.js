811c2a6849033ea443c54a677afff82b886afa91
StephenDK <stephendklein08@gmail.com>
2018-02-21 07:58:35 -0800

    Heroku mysql configure

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,26 +1,31 @@
 // Dependencies
 var Sequelize = require("sequelize");
+var mysql = require("mysql");
 
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     connection = mysql.createConnection({
+//         host: 'localhost',
+//         user: 'root',
+//         password: 'StephenDKlein360',
+//         database: 'sequelize_chirpy'
+//     });
+// };
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    sequelize = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'StephenDKlein360',
-        database: 'sequelize_chirpy'
+    var sequelize = new Sequelize("sequelize_chirpy", "root", "StephenDKlein360", {
+        host: "localhost",
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000
+        }
     });
-};
-
+}
 // Create a mysql connectiom with sequelize 
-var sequelize = new Sequelize("sequelize_chirpy", "root", "StephenDKlein360", {
-    host: "localhost",
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-    }
-});
+
 
 module.exports = sequelize;
